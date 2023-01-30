756c46f770eda9ff51e85575d624293c71be548f
akhror-valiev <ahror.valiev@gmail.com>
2023-01-30 17:08:45 +0500

    Create database

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE animals (
+	id INT PRIMARY KEY NOT NULL,
+	name VARCHAR(20) NOT NULL,
+	date_of_birth date NOT NULL,
+	escape_attempts INT,
+	neutered BOOLEAN,
+	weight_kg DECIMAL
+); 
+
+
