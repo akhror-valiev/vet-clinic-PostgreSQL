d1355321b5a965f8f1d6520f4c2a10a51725a9ac
akhror-valiev <ahror.valiev@gmail.com>
2023-02-02 11:00:12 +0500

    Update to the F-key

--- schema.sql
+++ schema.sql
@@ -12,3 +12,28 @@ ALTER TABLE animals
 ADD COLUMN species VARCHAR(30);
 
 
+--New tables owner and sepecies
+
+CREATE TABLE owners (
+    id INT PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INT  
+);
+
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+
+
+
+--- Change the animals table
+ALTER TABLE animals
+DROP COLUMN species,
+ADD COLUMN species_id INT,
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES species(id),
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
